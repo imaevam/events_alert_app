2a69b5e866f1ccac780a5737225f11d183c1c107
imaevam <imaevam@list.ru>
2020-10-08 12:49:19 +0300

    first commit

--- start/collect_movies.py
+++ start/collect_movies.py
@@ -2,38 +2,43 @@ import json
 import requests
 import pprint
 
-
 def get_payload(url):
     url = f"https://afisha.ru/{url}"
     return requests.get(url=url, headers={'Accept': 'application/json'}).json()
 
-def collect_concerts(concert_lst):
-    concerts_data = [] 
-    tiles = [tile for item in concert_lst for tile in item['Tiles']]
+
+def convert_date(date):
+    date = str(date)
+    out = date.replace("T"," ")
+    return out
+     
+
+def collect_movies(movie_lst): 
+    movie_data = [] 
+    tiles = [tile for item in movie_lst for tile in item['Tiles']]
     for tile in tiles:
         name = tile['Name']
         genre = tile['Badge']
-        date = tile['Notice']['Notice']
-        address = tile['Notice']['PlaceUrl']['Address']
+        date_start, date_finish = tile['ScheduleInfo']['MaxScheduleDate'], tile['ScheduleInfo']['MinScheduleDate'] 
+        date_min, date_max = convert_date(date_start), convert_date(date_finish)
         place = tile['Notice']['PlaceUrl']['Name']
         price = tile['ScheduleInfo']['MinPrice']
         url = tile['Url']
         description = tile['Verdict']
-        concerts_data.append({
+        movie_data.append({
             'name': name,
             'genre': genre,
-            'date': date,
-            'address': address,
+            'date_start': date_min,
+            'date_finish': date_max,
             'place': place,
             'price': price,
             'url': url, 
             'description': description
         })
-    return concerts_data
+    return movie_data
 
 if __name__ == "__main__":
-    concert_lst = get_payload('msk/concerts/')['Widget']['CardsCarousels']
-    result = collect_concerts(concert_lst)
+    movie_lst = get_payload('msk/cinema')['Widget']['CardsCarousels']
+    result = collect_movies(movie_lst)
     pp = pprint.PrettyPrinter(indent=2)
     pp.pprint(result)
-
--- start/collect_plays.py
+++ start/collect_plays.py
@@ -2,24 +2,29 @@ import json
 import requests
 import pprint
 
-
 def get_payload(url):
     url = f"https://afisha.ru/{url}"
     return requests.get(url=url, headers={'Accept': 'application/json'}).json()
 
-def collect_concerts(concert_lst):
-    concerts_data = [] 
-    tiles = [tile for item in concert_lst for tile in item['Tiles']]
+def convert_date(date):
+    date = str(date)
+    out = date.replace("T"," ")
+    return out
+
+def collect_plays(theatre_lst): 
+    theatre_data = [] 
+    tiles = [tile for item in theatre_lst for tile in item['Tiles']]
     for tile in tiles:
         name = tile['Name']
         genre = tile['Badge']
-        date = tile['Notice']['Notice']
+        date_max = tile['ScheduleInfo']['MaxScheduleDate'] 
+        date = convert_date(date_max)
         address = tile['Notice']['PlaceUrl']['Address']
         place = tile['Notice']['PlaceUrl']['Name']
         price = tile['ScheduleInfo']['MinPrice']
         url = tile['Url']
         description = tile['Verdict']
-        concerts_data.append({
+        theatre_data.append({
             'name': name,
             'genre': genre,
             'date': date,
@@ -29,11 +34,10 @@ def collect_concerts(concert_lst):
             'url': url, 
             'description': description
         })
-    return concerts_data
+    return theatre_data
 
 if __name__ == "__main__":
-    concert_lst = get_payload('msk/concerts/')['Widget']['CardsCarousels']
-    result = collect_concerts(concert_lst)
+    theatre_lst = get_payload('msk/theatre/')['Widget']['CardsCarousels']
+    result = collect_plays(theatre_lst)
     pp = pprint.PrettyPrinter(indent=2)
     pp.pprint(result)
-
